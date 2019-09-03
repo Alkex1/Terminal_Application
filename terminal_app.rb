83e1aeb956860ad5eb5822f7a274e1871d8423b1
Alkex1 <alikentwell@hotmail.com>
2019-09-03 16:48:52 +1000

    Changed to method, allowing me to reset loop

--- terminal_app.rb
+++ terminal_app.rb
@@ -1,53 +1,61 @@
-##myStorage = window.localStorage;
-puts "Hello what would you like to do (options: make file, make directory, quicknote)"
-user_input = gets.chomp
+require 'colorize'
 
-until user_input == "done"
+def note_app
+    puts "Hello what would you like to do (options: make file, make directory, quicknote)"
+        user_input = gets.chomp
+    until user_input == "done"
     
-    if user_input == "quicknote" #|| "q"
-        puts "quicknote:"
-        puts "type 'back' at any point to end task"
+        if user_input == "quicknote" #|| "q"
+            puts "quicknote:".colorize(:light_blue).underline
+            puts "type 'back' at any point to end task".colorize(:blue)
+            u_input1 = gets.chomp
 
-        u_input1 = gets.chomp
+            
+            if u_input1 == "back"
+                note_app
+            end
+
+            file = File.open("quicknote.txt", "a")
+            file << "\r" + "#{u_input1}"
+            file.close
+
         
-        if u_input1 == "back"
-            break
+
+        elsif user_input == "make file"
+            puts "What would you like to call it".colorize(:light_green).underline
+            puts "type 'back' at any point to end task".colorize(:green)
+
+            u_input2 = gets.chomp
+
+            if u_input2 == "back"
+                note_app
+            end
+            
+            File.new("#{u_input2}.txt", "w")
+            puts "Created #{u_input2}.txt"
+            
+            
+            
+        elsif user_input == "make directory"
+            
+            puts "Directory name:".colorize(:light_red).underline
+            puts "type 'back' at any point to end task".colorize(:light_red)
+            
+            u_input3= gets.chomp
+            if u_input3 == "back"
+                note_app
+            end
+            Dir.mkdir("#{u_input3}") unless File.exists?("#{u_input3}")
+        
+        elsif user_input == "exit"
+            exit
+        
+        else
+            puts "I'm sorry I didn't quite catch that"
+            note_app
         end
-
-        file = File.open("quicknote.txt", "a")
-        file << "\r" + "#{u_input1}"
-        file.close
-
-        
-
-    elsif user_input == "make file"
-        puts "What would you like to call it"
-        puts "type 'back' at any point to end task"
-
-        u_input2 = gets.chomp
-
-        if u_input2 == "back"
-            break
-        end
-
-        File.new("#{u_input2}.txt", "w")
-        puts "Created #{u_input2}.txt"
-        
-    
-
-    elsif user_input == "make directory"
-
-        puts "Directory name:"
-        puts "type 'back' at any point to end task"
-
-        u_input3= gets.chomp
-        if u_input3 == "back"
-            break
-        end
-        Dir.mkdir("#{u_input3}") unless File.exists?("#{u_input3}")
-        
-    else 
-        puts "I'm sorry I didn't quite catch that"
     end
 end
+
+note_app
 
